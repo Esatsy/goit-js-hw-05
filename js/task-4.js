4096dcf4be59002a85866685271ac71e19db6540
Esatsy <89773427+Esatsy@users.noreply.github.com>
2025-02-28 05:10:31 +0300

    tasks done

---
+++ js/task-4.js
@@ -0,0 +1,22 @@
+// Görev 4. Toplam bakiye
+
+// İki parametre alan bir getTotalBalanceByGender(users, gender) ok fonksiyonu yazın:
+
+// ilk parametre users - kullanıcı nesnelerinden oluşan bir dizi,
+// ikinci parametre gender - cinsiyeti saklayan bir dize.
+// Fonksiyon bir metot çağrı zinciri kullanmalı ve cinsiyeti (özellik gender) gender parametresinin değeriyle eşleşen kullanıcıların toplam bakiyesini (özellik balance) döndürmelidir.
+
+// Aşağıdaki kodu alın ve doğru çalışıp çalışmadığını kontrol etmek için fonksiyonunuzun tanımlanmasından sonra yapıştırın. Çalışmasının sonuçları konsolda görüntülenecektir.
+
+//yapamadim cozumu copy paste attim lms den 
+const getTotalBalanceByGender = (users, gender) => {
+  return users.reduce((total, user) => {
+    return user.gender === gender ? total + user.balance : total;
+  }, 0);
+};
+
+
+
+console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
+
+console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
